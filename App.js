1b660ca93ccaa954c29113b4a889a1c54854f6bd
Neda <nchangizi@bcit.ca>
2024-01-16 14:49:14 -0800

    Added a text input and state variable to control its value

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, View, Text } from "react-native";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
   const appName = "My awesome app";
+  const [text, setText] = useState("");
+
+  // callback handler
+  function changeTextHandler(changedText) {
+    console.log("user is typing ", changedText);
+    setText(changedText);
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} version={2} />
+      <TextInput
+        placeholder="Type something"
+        style={styles.input}
+        value={text}
+        onChangeText={changeTextHandler}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
@@ -18,5 +33,10 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: "purple",
+    width: "50%",
   },
 });
