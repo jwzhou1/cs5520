9993a7f767d0361b9c5d5bcc164a0736d0c6c3de
jwzhou1 <zhou.jiaw@northeastern.edu>
2024-01-16 13:44:28 -0800

    changed welcome message

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "My First App";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</Text>
+      <Text>Open up App.js to say hi {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
