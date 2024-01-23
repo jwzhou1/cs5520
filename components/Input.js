fb27e84224912cabc97dea4472d37b1e4c8f0962
Neda <nchangizi@bcit.ca>
2024-01-23 14:29:34 -0800

    added images to input.js

--- components/Input.js
+++ components/Input.js
@@ -1,4 +1,12 @@
-import { Button, Modal, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React, { useState } from "react";
 
 export default function Input({ inputHandler, modalVisible, dismissModal }) {
@@ -20,6 +28,13 @@ export default function Input({ inputHandler, modalVisible, dismissModal }) {
   return (
     <Modal visible={modalVisible}>
       <View style={styles.container}>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+          }}
+          style={styles.image}
+        />
+        <Image source={require("../assets/goal.png")} style={styles.image} />
         <TextInput
           placeholder="Type something"
           style={styles.input}
@@ -46,4 +61,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  image: { width: 100, height: 100 },
 });
