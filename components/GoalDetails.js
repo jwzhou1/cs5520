3825b5fa03335568f34adf51c7c54a24ec10e4b5
jwzhou1 <zhou.jiaw@northeastern.edu>
2024-02-13 13:14:36 -0800

    week5

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 
-export default function GoalDetails({ route }) {
-  console.log(route.params);
+export default function GoalDetails({ navigation, route }) {
+  const [warning, setWatning] = useState(false);
+
+  function warningHandler() {
+    console.log("warning");
+    setWatning(true);
+  }
+  // functions inside useEffect are called after the rendering
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <Button title="Warning" color="gray" onPress={warningHandler} />;
+      },
+    });
+  });
+
   return (
     <View>
       <Text>
         You are viewing details of {route.params.data.text} with id{" "}
         {route.params.data.id}
       </Text>
+      {warning && <Text style={{ color: "red" }}>WARNING</Text>}
     </View>
   );
 }
